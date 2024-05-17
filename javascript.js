84bb35d284446588e3cdd8e0704f12972d2a2a00
Leon <leondellafiore2016@gmail.com>
2024-05-17 16:38:50 -0300

    Carousel con imagenes en el background creado

--- javascript.js
+++ javascript.js
@@ -1,24 +1,19 @@
-var imgDiv = document.getElementById('images');
-const imagesFolder = '/images'
-const imgNames = ["img1","img2","img3","img4"];
-
+const imgId = ["img1","img2","img3","img4"];
 var currentIndex = 0;
-var paraph = document.createElement('p');
+//var paraph = document.createElement('p');
+//var frases = ["Sofware Engineering","Movile Apps","International","Work With us!"];
 
-imgDiv.classList.toggle('animacion');
+function changeanimation(){
+    var imgSalida = document.getElementById(imgId[currentIndex])
+    var imgEntrada = document.getElementById(imgId[currentIndex+1]);
+    imgSalida.classList.toggle('animacionSalida');
+    setTimeout(imgSalida.classList.toggle('hidden'), 5000);
+    imgEntrada.classList.toggle('hidden');
+    imgEntrada.classList.toggle('animacionEntrada');
+    currentIndex = (currentIndex + 1) % imgId.length; // Avanzar al siguiente índice circularmente
 
-var frases = ["Sofware Engineering","Movile Apps","International","Work With us!"];
 
+}
 function changeclass() {
-    imgDiv.classList.remove(imgNames[currentIndex]);
-    imgDiv.classList.toggle('animacion');
-    imgDiv.classList.toggle('animacion');
-    newText = document.createTextNode(frases[currentIndex]);
-    paraph.appendChild(newText);
-    imgDiv.appendChild(newText);
-    currentIndex = (currentIndex + 1) % imgNames.length; // Avanzar al siguiente índice circularmente
-    imgDiv.classList.add(imgNames[currentIndex]);
-    //setTimeout(imgDiv.innerText="",10000)
 }
 
-setInterval(changeclass,5000)
