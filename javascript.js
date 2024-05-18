8d03d15fc334dbfed9f1b1e331fd63c9bd37ef08
Leon <leondellafiore2016@gmail.com>
2024-05-18 16:47:15 -0300

    Vector de frases funciona, efecto apear disapear funciona en frase de prueba

--- javascript.js
+++ javascript.js
@@ -1,19 +1,35 @@
-const imgId = ["img1","img2","img3","img4"];
+const headersDiv = document.getElementById('headers')
+const spech = document.getElementById('spech')
+var headerChild = document.querySelector('p')
 var currentIndex = 0;
-//var paraph = document.createElement('p');
-//var frases = ["Sofware Engineering","Movile Apps","International","Work With us!"];
+var frases = ["Software Engineering","Movile Apps","International","Work With us!"];
 
-function changeanimation(){
-    var imgSalida = document.getElementById(imgId[currentIndex])
-    var imgEntrada = document.getElementById(imgId[currentIndex+1]);
-    imgSalida.classList.toggle('animacionSalida');
-    setTimeout(imgSalida.classList.toggle('hidden'), 5000);
-    imgEntrada.classList.toggle('hidden');
-    imgEntrada.classList.toggle('animacionEntrada');
-    currentIndex = (currentIndex + 1) % imgId.length; // Avanzar al siguiente índice circularmente
 
+headerChild.classList.toggle('visible');
+console.time('Intervalo');
+console.time('disApear')
+
+function changeText(){
+    console.timeLog('Intervalo')
+    setTimeout(headerChild.innerText="",2000)
+    var nodoTexto = document.createTextNode(frases[currentIndex]);
+    console.log(frases[currentIndex])
+    headerChild.appendChild(nodoTexto);
+    currentIndex = (currentIndex + 1) % frases.length;//[0..3]
 
 }
-function changeclass() {
+function disApear(){
+    spech.classList.toggle('visible')
+    spech.classList.toggle('fade-in-text');//Desaparece, en 2 segundos
+}
+function apear(){
+    spech.classList.toggle('visible')
 }
 
+function efectApear(){
+    apear();
+    setTimeout(disApear,2000)
+}
+changeText();
+setInterval(changeText,8000);
+setInterval(efectApear,4000)
